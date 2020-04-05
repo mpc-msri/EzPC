190b6656ea9360b98ff3582193711d988f07d973
shas19 <shubhamdugare@gmail.com>
2020-04-06 04:10:40 +0530

    change in temp file location for run_onnx_tf

--- Athos/ONNXCompiler/onnx_run_tf.py
+++ Athos/ONNXCompiler/onnx_run_tf.py
@@ -47,7 +47,7 @@ def main():
 	model = onnx.load(file_path)
 	model = preprocess_for_tf(model)
 
-	x = np.load(model_name + '_input.npy')
+	x = np.load('debug/' + model_name + '/' + model_name + '_input.npy')
 	x = x.astype(np.float32)
 
 	input_name = model.graph.input[0].name
@@ -60,7 +60,7 @@ def main():
 		model.graph.output.extend([intermediate_layer_value_info])
 		output = prepare(model).run(x) 
 		pred = getattr(output, intermediate_layer_value_info_name)
-		np.save(model_name + '_debug', pred)
+		np.save('debug/' + model_name + '/' + model_name + '_debug', pred)
 		with open('debug/onnx_debug.txt', 'w') as f:
 			f.write(common.numpy_float_array_to_float_val_str(pred))
 		print("Saving the onnx runtime intermediate output for " + intermediate_layer_value_info.name)
@@ -68,7 +68,7 @@ def main():
 
 	output = prepare(model).run(x) 
 	pred = getattr(output, output_name)
-	np.save(model_name + '_output', pred)
+	np.save('debug/' + model_name + '/' + model_name + '_output', pred)
 	with open('debug/onnx_output.txt', 'w') as f:
 			f.write(common.numpy_float_array_to_float_val_str(pred))
 	output_dims = common.proto_val_to_dimension_tuple(model.graph.output[0])
