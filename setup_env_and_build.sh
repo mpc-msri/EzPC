ac56f3a499ccd3225d64e0ac07a038bd0a579c38
Bhatu <prbhatu@microsoft.com>
2021-09-08 14:00:00 +0530

    add bc time to setup for docker

--- setup_env_and_build.sh
+++ setup_env_and_build.sh
@@ -28,7 +28,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt update
 sudo apt install -y build-essential cmake libgmp-dev libglib2.0-dev libssl-dev \
                     libboost-all-dev m4 python3.7 opam unzip bubblewrap \
-                    graphviz tmux
+                    graphviz tmux bc time
 
 #Install gcc 9
 sudo apt install -y gcc-9 g++-9
