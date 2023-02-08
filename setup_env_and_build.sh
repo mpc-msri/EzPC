8258b183728eb1f59df6d625419a1c07ce868172
Saksham Gupta <saksham1319@gmail.com>
2023-02-08 16:13:26 +0530

    dependency added

--- setup_env_and_build.sh
+++ setup_env_and_build.sh
@@ -116,7 +116,7 @@ sudo apt install -y python3.7-venv
 python3.7 -m venv mpc_venv
 source mpc_venv/bin/activate
 pip install -U pip
-pip install tensorflow==1.15.0 keras==2.3.0 scipy==1.1.0 matplotlib scikit-learn==0.24.2 torchvision
+pip install tensorflow==1.15.0 keras==2.3.0 scipy==1.1.0 matplotlib scikit-learn==0.24.2 torchvision opencv-python
 pip install onnx onnx-simplifier onnxruntime black
 pip install torchvision==0.13.1 idx2numpy==1.2.3
 pip install pytest pytest-cov 
