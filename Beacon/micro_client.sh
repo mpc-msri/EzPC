7b7cba47f031e6c5881338e56adcd5c63723758a
Anwesh Bhattacharya <anwesh.bh@gmail.com>
2023-03-01 00:43:58 +0530

    Added FP19 stuff

--- Beacon/micro_client.sh
+++ Beacon/micro_client.sh
@@ -5,6 +5,8 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/vsum-beacon r=2 sz1=2000 sz2=2000 add=$1
 printf "Beacon BF16\n"
 ./../SCI/build/bin/vsum-beacon r=2 sz1=2000 sz2=2000 add=$1 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/vsum-beacon r=2 sz1=2000 sz2=2000 add=$1 mbits=10 ebits=8
 
 printf "RUNNING : Dotprod\n"
 printf "Secfloat FP32\n"
@@ -13,6 +15,8 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/dotprod-beacon r=2 sz1=1000 sz2=1000 add=$1
 printf "Beacon BF16\n"
 ./../SCI/build/bin/dotprod-beacon r=2 sz1=1000 sz2=1000 add=$1 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/dotprod-beacon r=2 sz1=1000 sz2=1000 add=$1 mbits=10 ebits=8
 
 printf "RUNNING : Matmul\n"
 printf "Secfloat FP32\n"
@@ -21,6 +25,8 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/matmul-beacon r=2 sz1=100 sz2=100 sz3=100 add=$1
 printf "Beacon BF16\n"
 ./../SCI/build/bin/matmul-beacon r=2 sz1=100 sz2=100 sz3=100 add=$1 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/matmul-beacon r=2 sz1=100 sz2=100 sz3=100 add=$1 mbits=10 ebits=8
 
 printf "RUNNING : Softmax\n"
 printf "Secfloat FP32\n"
--- Beacon/micro_server.sh
+++ Beacon/micro_server.sh
@@ -5,6 +5,9 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/vsum-beacon r=1 sz1=2000 sz2=2000
 printf "Beacon BF16\n"
 ./../SCI/build/bin/vsum-beacon r=1 sz1=2000 sz2=2000 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/vsum-beacon r=1 sz1=2000 sz2=2000 mbits=10 ebits=8
+
 
 printf "RUNNING : Dotprod\n"
 printf "Secfloat FP32\n"
@@ -13,6 +16,9 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/dotprod-beacon r=1 sz1=1000 sz2=1000
 printf "Beacon BF16\n"
 ./../SCI/build/bin/dotprod-beacon r=1 sz1=1000 sz2=1000 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/dotprod-beacon r=1 sz1=1000 sz2=1000 mbits=10 ebits=8
+
 
 printf "RUNNING : Matmul\n"
 printf "Secfloat FP32\n"
@@ -21,6 +27,9 @@ printf "Beacon FP32\n"
 ./../SCI/build/bin/matmul-beacon r=1 sz1=100 sz2=100 sz3=100
 printf "Beacon BF16\n"
 ./../SCI/build/bin/matmul-beacon r=1 sz1=100 sz2=100 sz3=100 mbits=7 ebits=8
+printf "Beacon FP19\n"
+./../SCI/build/bin/matmul-beacon r=1 sz1=100 sz2=100 sz3=100 mbits=10 ebits=8
+
 
 printf "RUNNING : Softmax\n"
 printf "Secfloat FP32\n"
