7313a8288368a4d6e4f7182470229d5bee296505
Saksham Gupta <saksham1319@gmail.com>
2023-03-28 19:21:10 +0530

    tranfer progress added to script

--- sytorch/scripts/dealer.py
+++ sytorch/scripts/dealer.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 import threading
 import subprocess
+import os
 
 PORT_SERVER = 9000
 PORT_CLIENT = 9001
@@ -16,9 +17,8 @@ class FileServer(http.server.SimpleHTTPRequestHandler):
         self.files_served_to_client = 0
         self.files_served_to_server = 0
 
-    def do_GET(self):
-        super().do_GET()
-        self.end_headers()
+    def end_headers(self):
+        super().end_headers()
         print(f"File served to {self.server.server_address[1]}")
         if self.server.server_address[1] == PORT_SERVER:
             FileServer.files_served_to_server += 1
@@ -36,6 +36,33 @@ class FileServer(http.server.SimpleHTTPRequestHandler):
             subprocess.run(["./generate_keys", "1"])
             FileServer.files_served_to_client = 0
             FileServer.files_served_to_server = 0
+
+    def do_GET(self):
+        f = self.send_head()
+        if f:
+            try:
+                self.send_file(f)
+            except BrokenPipeError:
+                # Client disconnected before the file was fully sent
+                pass
+            f.close()
+
+    def send_file(self, f):
+        CHUNK_SIZE = 16 * 1024
+        total_size = os.fstat(f.fileno()).st_size
+        bytes_sent = 0
+
+        while True:
+            chunk = f.read(CHUNK_SIZE)
+            if not chunk:
+                break
+
+            self.wfile.write(chunk)
+            bytes_sent += len(chunk)
+            progress = min(100, int(100 * bytes_sent / total_size))
+            print(f"\rTransfer progress: [{progress:3}%] ", end="", flush=True)
+
+        print("\rTransfer complete.                 ")
 
 
 def serve_files(port, server_type):
